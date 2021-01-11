236756b92c76648ec037bc160f8dbc9985d63f1b
husamrayan <husam@oratic.com>
2021-01-11 13:21:33 +0200

    coment line 629

--- js/fontawesome.js
+++ js/fontawesome.js
@@ -626,7 +626,7 @@
     }
 
     var style = DOCUMENT.createElement('style');
-    style.setAttribute('type', '');
+    // style.setAttribute('type', 'text/css');
     style.innerHTML = css;
     var headChildren = DOCUMENT.head.childNodes;
     var beforeChild = null;
